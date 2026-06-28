c34a8eb2e748821448ab0936a929cccd19dd15c2
Kandula Mohana Varsha Sri <kandulamohana14@gmail.com>
2026-06-29 00:21:05 +0530

    Update CorsConfig.java

--- src/main/java/com/codingshuttle/projects/airBnbApp/config/CorsConfig.java
+++ src/main/java/com/codingshuttle/projects/airBnbApp/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.codingshuttle.projects.airBnbApp.config;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,20 +13,27 @@ public class CorsConfig {
     @Value("${frontend.url}")
     private String frontendUrl;
 
+    @PostConstruct
+    public void init() {
+        System.out.println("Frontend URL = " + frontendUrl);
+    }
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(frontendUrl, 
-                                "http://localhost:3000", 
-                                "http://localhost:3001", 
-                                "http://localhost:3002", 
-                                "http://localhost:3003", 
-                                "http://localhost:3004", 
-                                "http://localhost:3005", 
-                                "http://localhost:5173")
+                        .allowedOrigins(
+                                frontendUrl,
+                                "http://localhost:3000",
+                                "http://localhost:3001",
+                                "http://localhost:3002",
+                                "http://localhost:3003",
+                                "http://localhost:3004",
+                                "http://localhost:3005",
+                                "http://localhost:5173"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                         .allowedHeaders("*")
                         .allowCredentials(true);
